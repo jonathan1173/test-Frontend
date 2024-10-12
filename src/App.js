58c031c2a1e252bccffcb0eb6d480b1389bb1680
Jonathan <Jonathan106e@gmail.com>
2024-10-12 14:24:57 -0600

    archivos iniciales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import Welcome from './components/Welcome';
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel'
+import Gallery from './components/Gallery'
+import Footer from './components/Footer'
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Carousel/>
+      <Welcome/>
+      <Gallery/>
+      <Footer/>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
